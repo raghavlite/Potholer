3dde1f9f984c2f2106b681b1758180a4b9be96b7
raghavlite <raghavlite@gmail.com>
2014-12-13 02:34:30 +0530

    added request part

--- potHoler/src/com/example/potholer/MainActivity.java
+++ potHoler/src/com/example/potholer/MainActivity.java
@@ -2,8 +2,22 @@ package com.example.potholer;
 
 
 
-import info.androidhive.jsonparsing.MainActivity;
-import info.androidhive.jsonparsing.ServiceHandler;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
@@ -259,7 +273,10 @@ GooglePlayServicesClient.OnConnectionFailedListener {
 			public void onLocationChanged(Location location) {
 				// TODO Auto-generated method stub
 				
-				Toast.makeText(getApplicationContext(), "Location"+location.getLatitude(),Toast.LENGTH_SHORT).show();
+				
+				
+				new AsyncTaskActivity2().execute(""+location.getLatitude(),""+location.getLongitude());
+				
 				
 				Log.d("Raghav","raghav");
 			}
@@ -292,81 +309,90 @@ GooglePlayServicesClient.OnConnectionFailedListener {
 
 
 
-	private class GetContacts extends AsyncTask<Void, Void, String> {
+	private class AsyncTaskActivity2 extends AsyncTask<String, Void, String> {
 
-		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
+		String url="http://192.168.1.139:7010/getRoadQuality";
+		
+		
+//		Activity mActivity;
+//		    public AsyncTaskActivity2(Activity activity)
+//		    {
+//		         super();
+//		         this.mActivity=activity;
+//		    }
+//		    
+		    
+		    
+		    
+				@Override
+			protected String doInBackground(String... params) {
+				
+				String response = null;
+				
+				
+				HttpEntity httpEntity = null;
+				HttpResponse httpResponse = null;
+				
+				 HttpClient httpclient = new DefaultHttpClient();
+				    HttpPost httppost = new HttpPost(url);
 	
-		}
-
-		@Override
-		protected String doInBackground(Void... arg0) {
-			// Creating service handler class instance
-			ServiceHandler sh = new ServiceHandler();
-
-			// Making a request to url and getting response
-			String jsonStr = sh.makeServiceCall(url, ServiceHandler.GET);
-
-			Log.d("Response: ", "> " + jsonStr);
-
-//			if (jsonStr != null) {
-//				try {
-//					JSONObject jsonObj = new JSONObject(jsonStr);
-//					
-//					// Getting JSON Array node
-//					contacts = jsonObj.getJSONArray(TAG_CONTACTS);
-//
-//					// looping through All Contacts
-//					for (int i = 0; i < contacts.length(); i++) {
-//						JSONObject c = contacts.getJSONObject(i);
-//						
-//						String id = c.getString(TAG_ID);
-//						String name = c.getString(TAG_NAME);
-//						String email = c.getString(TAG_EMAIL);
-//						String address = c.getString(TAG_ADDRESS);
-//						String gender = c.getString(TAG_GENDER);
-//
-//						// Phone node is JSON Object
-//						JSONObject phone = c.getJSONObject(TAG_PHONE);
-//						String mobile = phone.getString(TAG_PHONE_MOBILE);
-//						String home = phone.getString(TAG_PHONE_HOME);
-//						String office = phone.getString(TAG_PHONE_OFFICE);
-//
-//						// tmp hashmap for single contact
-//						HashMap<String, String> contact = new HashMap<String, String>();
-//
-//						// adding each child node to HashMap key => value
-//						contact.put(TAG_ID, id);
-//						contact.put(TAG_NAME, name);
-//						contact.put(TAG_EMAIL, email);
-//						contact.put(TAG_PHONE_MOBILE, mobile);
-//
-//						// adding contact to contact list
-//						contactList.add(contact);
-//					}
-//				} catch (JSONException e) {
-//					e.printStackTrace();
-//				}
-//			} else {
-//				Log.e("ServiceHandler", "Couldn't get any data from the url");
-//			}
-
-			return jsonStr;
-		}
-
-		@Override
-		protected void onPostExecute(String result) {
-			super.onPostExecute(result);
+				    try {
+				        // Add your data
+				    	String[] rv={params[0],params[1]};
+				    	
+				    	String str1 = Arrays.toString(rv);  
+				    	
+				    	
+				    	
+				    	
+				    	
+				        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+				        nameValuePairs.add(new BasicNameValuePair("coordinate", str1));
+				      
+				        
+				        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 	
+				        // Execute HTTP Post Request
+				        httpResponse = httpclient.execute(httppost);
+				        
+				        httpEntity = httpResponse.getEntity();
+						response = EntityUtils.toString(httpEntity);
+						
+						Log.d("response", response);
+						
+				        
+				    } catch (ClientProtocolException e) {
+				        // TODO Auto-generated catch block
+				    	e.printStackTrace();
+				    } catch (IOException e) {
+				        // TODO Auto-generated catch block
+				    	e.printStackTrace();
+				    }
+				
+				
+				
 			
-			Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
-			Log.d("RAGHAV", result);
-
+				
+				return response;
+	
+			}
+		    
+		    
+				@Override
+				protected void onPostExecute(String result) {
+					
+					
+				Log.d("raghav", "done posting");
+					
+					//shareRegidTask = null;
+					//Toast.makeText(getApplicationContext(), result,
+						//	Toast.LENGTH_LONG).show();
+				}
+		    
+		    
+		    
 		}
-
-	}
-
+	
 	
 	
 	
